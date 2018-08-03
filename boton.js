7f52356a1ce696b69f73bf1597974835d24b8462
Marisol Davila Escobar <marisol.davila@correounivalle.edu.co>
2018-08-02 20:14:48 -0500

    PESTAÑA 6 - STEP 3

--- boton.js
+++ boton.js
@@ -88,3 +88,50 @@ $("[class^=slide]").on("click", "li", function(){
   $(".slide--content").children().removeClass("slide--content__pane--is-visible");
   paneIndex.addClass("slide--content__pane--is-visible");
 });
+
+
+/* CARRUSEL PESTAÑA 6*/
+var carousel = $('#carousel'),
+    threshold = 150,
+    slideWidth = 500,
+    dragStart, 
+    dragEnd;
+
+$('#next').click(function(){ shiftSlide(-1) })
+$('#prev').click(function(){ shiftSlide(1) })
+
+carousel.on('mousedown', function(){
+  if (carousel.hasClass('transition')) return;
+  dragStart = event.pageX;
+  $(this).on('mousemove', function(){
+    dragEnd = event.pageX;
+    $(this).css('transform','translateX('+ dragPos() +'px)')
+  })
+  $(document).on('mouseup', function(){
+    if (dragPos() > threshold) { return shiftSlide(1) }
+    if (dragPos() < -threshold) { return shiftSlide(-1) }
+    shiftSlide(0);
+  })
+});
+
+function dragPos() {
+  return dragEnd - dragStart;
+}
+
+function shiftSlide(direction) {
+  if (carousel.hasClass('transition')) return;
+  dragEnd = dragStart;
+  $(document).off('mouseup')
+  carousel.off('mousemove')
+          .addClass('transition')
+          .css('transform','translateX(' + (direction * slideWidth) + 'px)'); 
+  setTimeout(function(){
+    if (direction === 1) {
+      $('.slides:first').before($('.slides:last'));
+    } else if (direction === -1) {
+      $('.slides:last').after($('.slides:first'));
+    }
+    carousel.removeClass('transition')
+    carousel.css('transform','translateX(0px)'); 
+  },700)
+}
