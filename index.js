c2f0c43ee432128283ba89c2aa568d7876e31e47
Marisol Davila Escobar <marisol.davila@correounivalle.edu.co>
2018-08-15 22:51:45 -0500

    INTRODUCCION - EFECTO

--- index.js
+++ index.js
@@ -88,3 +88,19 @@ $(document).ready(function() {
             });
             
         })();
+
+
+
+var myString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+var myArray = myString.split("");
+var loopTimer;
+
+function looper() {
+  if(myArray.length > 0){
+    document.getElementById("typingText").innerHTML += myArray.shift();
+  } else {
+    clearTimeout(loopTimer);
+  }
+  loopTimer = setTimeout('looper()', 100);
+}
+looper();
