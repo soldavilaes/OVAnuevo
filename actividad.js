51091cf76ee9d67d954e0dd42d32817298dd2897
pacovi0606 <40521162+pacovi0606@users.noreply.github.com>
2018-09-21 21:49:50 -0500

    actividad
    pestañas

--- actividad.js
+++ actividad.js
@@ -53,3 +53,24 @@ $('.rerun-button').click(function(){
                           .siblings().removeClass('is-active');
  $(this).hide();
 });
+
+/*--------------------------
+*PESTAÑAS
+---------------------------*/
+
+// show first content by default
+$('#tabs-nav li:last-child').addClass('active');
+$('.contentt').hide();
+$('.contentt:first').show();
+
+// click function
+$('#tabs-nav li').click(function(){
+  $('#tabs-nav li').removeClass('active');
+  $(this).addClass('active');
+  $('.contentt').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+  
+});
