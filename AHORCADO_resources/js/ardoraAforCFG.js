762604323a4288c523728fc042bdfea19eb5ffc5
Marisol Davila Escobar <marisol.davila@correounivalle.edu.co>
2018-09-23 19:11:56 -0500

    ACTIVIDADES EN CONFLICTO

---
+++ AHORCADO_resources/js/ardoraAforCFG.js
@@ -0,0 +1,33 @@
+
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=1; attempts=0; attemptsMax=1;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=0
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=true;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Arial, Helvetica, sans-serif";
+var fActi="Arial, Helvetica, sans-serif";
+var fEnun="Arial, Helvetica, sans-serif";
+var timeOnMessage=5; messageOk="Correcto!!!"; messageTime=""; messageError="Inconrrecto"; messageErrorG="Inconrrecto"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="QUhPUkNBRE8"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["UkVDVVJTSVZJREFE","","","","","","","","",""];imaW=["","","","","","","","","",""];queW=["","","","","","","","","",""];
+var auW=["","","","","","","","","",""];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ "; wordsG=[]; indexG=0; actMaxWidth=550; indexWord=0; totalWord=0;
+var imageW=[];questionW=[];audioW=[];profG=0; dirMedia="AHORCADO_resources/media/"; textBNext="";
